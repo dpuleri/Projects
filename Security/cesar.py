81e0fc4a380aa6a715a513adfb6b5ce26c04979a
dpuleri <daniel.puleri@gmail.com>
2014-04-13 01:16:33 -0400

    Fixed a line.

--- Security/cesar.py
+++ Security/cesar.py
@@ -1,4 +1,4 @@
 ##returns the cesar cipher of a string shifted n places
 ##did it in one line for fun
 def cesar(st, n):
-    return ''.join(chr(ord(c) + (-(n%25) if ord(c)+n > (ord('z') or ord('Z')) else (n%25))) for c in st)
+    return ''.join(chr(ord(c) + (-(n%25) if ord(c)+(n%25) > (ord('z') or ord('Z')) else (n%25))) for c in st)
